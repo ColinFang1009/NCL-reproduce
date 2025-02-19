ddb64febc98c8ec5bc30ff4e5573f19c9fb9676c
Weike Fang <weikefan@usc.edu>
2025-02-18 18:10:56 -0800

    run

--- NCL-changed/code/run_officehome.sh
+++ NCL-changed/code/run_officehome.sh
@@ -13,8 +13,8 @@ gpu_id=$1
 seed=$2
 
 # Set the variable s (in this case, hardcoded to 0)
-# original: s<=3
-for ((s=0;s<=0;s++))
+# original: s=0; s<=3
+for ((s=0;s<=3;s++))
 do
 
 ## Run the Python scripts with the provided arguments
@@ -24,8 +24,23 @@ echo "--------------------------------------------------------------------------
 echo "Domain Adaptation from Source to Target"
 echo "Running NCL"
 echo "-------------------------------------------------------------------------------------------------"
+
 python image_NCL.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/t_NCL$seed" --output_src "ckps/s$seed" --s $s --k 3 --cls_par 0.1 --max_epoch 50 --if_teacher 1
 
 #python image_mixmatch.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/mm$seed" --s $s --output_tar "ckps/t$seed" --cls_par 0.3 --ssl 0.6 --choice ent --ps 0.0 --max_epoch 50
 
 done
+
+
+# # Set the variable s (in this case, hardcoded to 0)
+# for ((s=0;s<=3;s++))
+# do
+
+# ## Run the Python scripts with the provided arguments
+# python image_source.py --gpu_id $gpu_id --seed $seed --output "ckps/s$seed" --dset office-home --max_epoch 50 --s $s
+
+# python image_NCL.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/t_NCL$seed" --output_src "ckps/s$seed" --s $s --k 3 --cls_par 0.1 --max_epoch 50
+
+# #python image_mixmatch.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/mm$seed" --s $s --output_tar "ckps/t$seed" --cls_par 0.3 --ssl 0.6 --choice ent --ps 0.0 --max_epoch 50
+
+# done
