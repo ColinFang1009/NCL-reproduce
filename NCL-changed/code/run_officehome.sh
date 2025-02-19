8ce2b13e01e012378e41da0db63a78dcf5039615
Weike Fang <weikefan@usc.edu>
2025-02-18 18:15:25 -0800

    remove checkpoints

--- NCL-changed/code/run_officehome.sh
+++ NCL-changed/code/run_officehome.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# background run: nohup bash run_officehome.sh 0 2023 > officehome.log 2>&1 &
+# background run: nohup bash run_officehome.sh 0 95 > officehome_ema95.log 2>&1 &
 
 # normal run: bash run_officehome.sh 0 2023 
 # Make sure that the script receives two arguments: GPU_ID and SEED
@@ -18,6 +18,7 @@ for ((s=0;s<=3;s++))
 do
 
 ## Run the Python scripts with the provided arguments
+# Step 1: Train on source "Art" - domain A
 python image_source.py --gpu_id $gpu_id --seed $seed --output "ckps/s$seed" --dset office-home --max_epoch 50 --s $s
 
 echo "-------------------------------------------------------------------------------------------------"
@@ -25,7 +26,8 @@ echo "Domain Adaptation from Source to Target"
 echo "Running NCL"
 echo "-------------------------------------------------------------------------------------------------"
 
-python image_NCL.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/t_NCL$seed" --output_src "ckps/s$seed" --s $s --k 3 --cls_par 0.1 --max_epoch 50 --if_teacher 1
+# Step 2: Domain Adaptation from Source to Target (Source: Art, Target: Clipart)
+python image_NCL.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/t_NCL$seed" --output_src "ckps/s$seed" --s $s --k 3 --cls_par 0.1 --max_epoch 100 --if_teacher 1
 
 #python image_mixmatch.py --gpu_id $gpu_id --seed $seed --da uda --dset office-home --output "ckps/mm$seed" --s $s --output_tar "ckps/t$seed" --cls_par 0.3 --ssl 0.6 --choice ent --ps 0.0 --max_epoch 50
 
